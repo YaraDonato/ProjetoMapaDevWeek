6ef442b56729e9d4189fc8df4720f03389777839
YaraDonato <sobrinhadosgames@gmail.com>
2022-05-21 03:11:56 -0300

    Create index.js

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+/* 
+MANEIRA ERRADA
+
+const homemDeFerro = document.getElementById('homem-de-ferro')
+
+homemDeFerro.addEventListener('mouseenter', () => { 
+    homemDeFerro.classList.add('selecionado')
+})
+
+const hulk = document.getElementById('hulk')
+
+hulk.addEventListener('mouseenter', () => { 
+    hulk.classList.add('selecionado')
+})
+*/
+
+// MANEIRA CERTA, aqui voce esta adicionando e removendo do anterior
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        const idSelecionado = personagem.attributes.id.value;
+
+        if(idSelecionado === 'ultron') return;
+
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+        
+        personagem.classList.add('selecionado')
+
+    
+
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./MiniCurso/src/imagens/${idSelecionado}.png`;
+
+        const nomeJogador1 = document.getElementById('nome-jogador-1')
+        //nomeJogador1.innerHTML = 'batata'
+
+        const nomeSelecionado =  personagem.getAttribute('data-name');
+
+        nomeJogador1.innerHTML = nomeSelecionado;
+    });
+});
+
